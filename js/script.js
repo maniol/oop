afbfc17d21ae1932883f1615e912d220186131f4
magda <magdalena.aniol@gmail.com>
2018-06-20 09:52:14 +0200

    add button with self

--- js/script.js
+++ js/script.js
@@ -30,3 +30,22 @@ var onePlusOne = new Phone('OnePlus', 70, 'black', true, 'One');
 
 onePlusOne.printInfo();
 samsungGalaxyS6.comparePrice(iPhone6S);
+
+// zadanie 11.5
+
+function Button(text) {
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.element = document.createElement('button');
+		this.element.innerText = this.text;
+		this.element.addEventListener('click', function() { alert(self.text);});
+		document.body.appendChild(this.element);
+	}
+}
+
+var btn1 = new Button('Hello!');
+btn1.create();
